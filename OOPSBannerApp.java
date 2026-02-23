bef1adac18f53e1e8a11f947a895529dc9980a2a
Ojas Gangrade <ojasgangrade@gmail.com>
2026-02-24 00:11:21 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+/**
+ * @author Ojas Gangrade
+ * @version 1.0
+ * OOPSBannerApp - Displays the text "OOPS" to the console
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
